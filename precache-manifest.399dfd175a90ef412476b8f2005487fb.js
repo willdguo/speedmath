7bfd10b81e0d40cb806f2b5eb5f2ea81c3f4cd22
WowliamGuo <64669373+WowliamGuo@users.noreply.github.com>
2023-04-26 18:22:04 -0400

    Updates

---
+++ precache-manifest.399dfd175a90ef412476b8f2005487fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff7a5961ca0a4345e7ae",
+    "url": "/speedmath/static/js/main.ff7a5961.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/speedmath/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "d4f2921df9c288598701",
+    "url": "/speedmath/static/js/runtime~main.d4f2921d.js"
+  },
+  {
+    "revision": "e81c6acc51bb335a968f078ddce9a3d9",
+    "url": "/speedmath/index.html"
+  }
+];
